3caaeff45a95d0e22f6df744ecb536398db3e9e0
Niklas Schöllhorn <schoellhorn.niklas@gmail.com>
2020-04-10 17:17:54 +0200

    Implement liquibase changes for new relational schema
    Also, a docker-compose.yaml file was added, containing a development postgres database with postgis enabled.

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,12 +24,15 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	implementation("com.google.firebase:firebase-admin:6.12.2")
+	implementation("org.liquibase:liquibase-core:3.6.3")
+	runtimeOnly("org.postgresql:postgresql:42.2.12")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
