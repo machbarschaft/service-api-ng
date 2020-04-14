968ae9e9aeb7159039eae5b1281d69ac5e7b1f9a
fabian brueck <fabianpunktbrueck@gmail.com>
2020-04-14 14:47:40 +0200

    deal with blocking call to firebase auth

--- src/main/kotlin/com/colivery/serviceaping/security/FirebaseAuthenticationConverter.kt
+++ src/main/kotlin/com/colivery/serviceaping/security/FirebaseAuthenticationConverter.kt
@@ -1,12 +1,16 @@
 package com.colivery.serviceaping.security
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseToken
 import org.springframework.http.HttpHeaders
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.web.server.authentication.ServerAuthenticationConverter
 import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
+import java.util.concurrent.Callable
+
 
 class FirebaseAuthenticationConverter : ServerAuthenticationConverter {
 
@@ -14,21 +18,24 @@ class FirebaseAuthenticationConverter : ServerAuthenticationConverter {
 
         val token: String? = exchange.request.headers.getFirst(HttpHeaders.AUTHORIZATION)?.let { getToken(it) }
 
-        return when (val firebaseUser = token?.let { getFirebaseUser(it) }) {
-            null -> Mono.empty()
-            else -> Mono.just(UsernamePasswordAuthenticationToken(firebaseUser.uid, token))
-        }
+        val fireBaseToken = token?.let { getFirebaseUser(it) } ?: Mono.empty()
+
+        return fireBaseToken.map { UsernamePasswordAuthenticationToken(it.uid, token) }
+
     }
 
     private fun getToken(authHeader: String?) = authHeader?.let {
         when {
-            it.startsWith("Bearer ") -> it.drop(    7)
+            it.startsWith("Bearer ") -> it.drop(7)
             else -> null
         }
     }
 
-    private fun getFirebaseUser(token: String?) = token?.let {
-        FirebaseAuth.getInstance().verifyIdToken(it)
+    private fun getFirebaseUser(token: String): Mono<FirebaseToken> {
+        return Mono.fromCallable(Callable {
+            FirebaseAuth.getInstance().verifyIdToken(token)
+        }).publishOn(Schedulers.elastic())
+
     }
 
 }
