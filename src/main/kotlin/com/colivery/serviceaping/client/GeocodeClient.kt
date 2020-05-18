78c06e3042e07a392bd15fcb21ec123c1f9b9f56
fabian brueck <fabianpunktbrueck@gmail.com>
2020-05-18 20:52:18 +0200

    google maps as primary geocoding provider

--- src/main/kotlin/com/colivery/serviceaping/client/GeocodeClient.kt
+++ src/main/kotlin/com/colivery/serviceaping/client/GeocodeClient.kt
@@ -1,14 +1,8 @@
 package com.colivery.serviceaping.client
 
-import com.colivery.serviceaping.client.esri.EsriAddress
-import com.colivery.serviceaping.client.esri.EsriConfiguration
 import com.neovisionaries.i18n.CountryCode
-import org.springframework.http.HttpStatus
-import org.springframework.util.LinkedMultiValueMap
 import org.springframework.web.reactive.function.client.ClientResponse
-import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
-import reactor.kotlin.core.publisher.toMono
 
 interface GeocodeClient {
 
@@ -16,5 +10,5 @@ interface GeocodeClient {
 
     fun errorToException(response: ClientResponse): Mono<GeocodeClientException> =
             response.bodyToMono(String::class.java)
-                    .flatMap{ Mono.error<GeocodeClientException>(GeocodeClientException(it, response.statusCode().name)) }
+                    .flatMap { Mono.error<GeocodeClientException>(GeocodeClientException(it, response.statusCode().name)) }
 }
