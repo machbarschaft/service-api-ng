0ca01bc1b00855cccc625e0c5913a1d286d2a0ee
fabian brueck <fabianpunktbrueck@gmail.com>
2020-05-26 08:51:29 +0200

    fixed sql constraints and dto of user

--- src/main/kotlin/com/colivery/serviceaping/rest/v1/dto/user/CreateUserDto.kt
+++ src/main/kotlin/com/colivery/serviceaping/rest/v1/dto/user/CreateUserDto.kt
@@ -11,20 +11,20 @@ import javax.validation.constraints.NotNull
 @Validated
 data class CreateUserDto(
         @field:NotEmpty(groups = [App::class])
-        val firstName: String,
+        val firstName: String?,
         @field:NotEmpty(groups = [App::class])
-        val lastName: String,
+        val lastName: String?,
         @field:NotEmpty(groups = [App::class])
-        val street: String,
+        val street: String?,
         @field:NotEmpty(groups = [App::class])
-        val streetNo: String,
+        val streetNo: String?,
         @field:NotEmpty
         val zipCode: String,
         @field:NotEmpty
         val city: String,
         @field:NotEmpty(groups = [App::class])
         @field:Email(groups = [App::class])
-        val email: String,
+        val email: String?,
         @field:NotNull
         val location: GeoPointResource,
         @field:NotEmpty
