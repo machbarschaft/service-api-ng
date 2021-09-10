3b70705ccbbcda0052320dbc93cf2d79ad5fb933
Basler182 <showdialog@web.de>
2021-09-10 22:22:42 +0200

    feat TECH-233 endpoint returning all accepted help requests of a user by a specific request status

--- src/main/kotlin/com/colivery/serviceaping/persistence/repository/HelpRequestRepository.kt
+++ src/main/kotlin/com/colivery/serviceaping/persistence/repository/HelpRequestRepository.kt
@@ -1,5 +1,6 @@
 package com.colivery.serviceaping.persistence.repository
 
+import com.colivery.serviceaping.persistence.RequestStatus
 import com.colivery.serviceaping.persistence.entity.HelpRequestEntity
 import com.colivery.serviceaping.persistence.entity.UserEntity
 import org.springframework.data.jpa.repository.Query
@@ -11,5 +12,5 @@ interface HelpRequestRepository : CrudRepository<HelpRequestEntity, UUID> {
     @Query("from HelpRequestEntity where requestStatus <> 'DELETED'")
     fun findAllWithoutDeleted(): Set<HelpRequestEntity>
     fun findAllByAdminUser(adminUser: UserEntity): Set<HelpRequestEntity>
-
+    fun findAllByHelperAndRequestStatus(helper: UserEntity, requestStatus: RequestStatus): Set<HelpRequestEntity>
 }
